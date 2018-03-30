3b608666be65ccd8aef884e0cb6cbcb2b7c24afa
Kyo <Ismail Ahmad>
2018-03-30 22:56:23 +0900

    add payment fields to orders table

---
+++ database/migrations/2018_03_30_134702_add_payment_fields_to_orders.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('payment_provider')->nullable();
+            $table->string('payment_ref')->nullable();
+            $table->string('payment_status')->default('unpaid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn(['payment_provider', 'payment_ref', 'payment_status']);
+        });
+    }
+};
