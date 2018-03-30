71ae257fa6da2f4b41a3cc493a7735dba521ee7f
Kyo <Ismail Ahmad>
2018-03-30 22:26:43 +0900

    fix error - $products in cart page

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -1,10 +1,36 @@
 @extends('layouts.app')
 
-@section('title','Cart')
+@section('title', 'Cart')
 
 @section('content')
-@foreach($products as $product)
-{{ $product->name }} ({{ $cart[$product->id]['qty'] }})
-@endforeach
-<a href="{{ route('checkout') }}">Checkout</a>
+
+<h1>Your Cart</h1>
+
+@if(empty($cart))
+    <p>Your cart is empty.</p>
+@else
+    <table>
+        <thead>
+            <tr>
+                <th>Product</th>
+                <th>Qty</th>
+                <th>Price</th>
+                <th>Total</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($cart as $item)
+            <tr>
+                <td>{{ $item['name'] }}</td>
+                <td>{{ $item['qty'] }}</td>
+                <td>RM {{ number_format($item['price'], 2) }}</td>
+                <td>RM {{ number_format($item['qty'] * $item['price'], 2) }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
+    <a href="{{ route('checkout') }}">Proceed to Checkout</a>
+@endif
+
 @endsection
