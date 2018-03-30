8c8ee08606d5ac8445efeaa678ec737319dfb573
Kyo <Ismail Ahmad>
2018-03-30 22:57:58 +0900

    add pay now button on order success page

--- resources/views/pages/success.blade.php
+++ resources/views/pages/success.blade.php
@@ -1,7 +1,45 @@
 @extends('layouts.app')
 
-@section('title','Success')
+@section('title', 'Order Success')
 
 @section('content')
-<h1>Order Successful</h1>
+
+<h1>Thank you for your order!</h1>
+
+<p>
+    <strong>Order No:</strong> {{ $order->order_no }}
+</p>
+
+<table>
+    <thead>
+        <tr>
+            <th>Product ID</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Subtotal</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($order->items as $item)
+        <tr>
+            <td>{{ $item->product_id }}</td>
+            <td>{{ $item->quantity }}</td>
+            <td>RM {{ number_format($item->price, 2) }}</td>
+            <td>
+                RM {{ number_format($item->price * $item->quantity, 2) }}
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+
+<p>
+    <strong>Total:</strong>
+    RM {{ number_format($order->total, 2) }}
+</p>
+
+<a href="{{ route('payment.pay', $order->order_no) }}">
+    Pay Now
+</a>
+
 @endsection
