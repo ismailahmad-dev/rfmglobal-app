42bda9ff30644003f823e51c6cdaf8f89adb3039
Kyo <Ismail Ahmad>
2018-03-30 23:08:43 +0900

    auto-generate product slug on create

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = [
-        'name','slug','description','price','image','is_private'
+        'name',
+        'slug',
+        'price',
+        'is_active',
     ];
+
+    protected static function booted(): void
+    {
+        static::creating(function (Product $product) {
+            if (empty($product->slug)) {
+                $product->slug = Str::slug($product->name);
+            }
+        });
+    }
 }
